ecd92c9c545ed69e62bfbec9b23225ced5670749
Yoon <actominal123@gmail.com>
2023-12-03 23:49:39 -0800

    Add password view functionality

--- client/src/components/InputGroup/InputGroup.jsx
+++ client/src/components/InputGroup/InputGroup.jsx
@@ -1,21 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './InputGroup.module.css'
+import { IoMdEye } from "react-icons/io";
+import { IoMdEyeOff } from "react-icons/io";
+
 
 export default function InputGroup({placeholder,value,setValue,error,type,onChange}) {
+    const [isPasswordVisible, setIsPasswordVisible] = useState(false);
+
     const handleChange = (e) => {
         setValue(e.target.value);
     }
+
+    const togglePasswordVisibility = () => {
+        setIsPasswordVisible(!isPasswordVisible);
+    };
+
     return (
         <div className={styles.container}>
             <input
                 className={`${styles.inputContainer} ${error && styles.inputError}`} 
-                type={type}
+                type={isPasswordVisible ? 'text' : type} 
                 placeholder={placeholder}
                 value={value}
                 onChange={onChange ? onChange : handleChange}
                 required
                 autoComplete="off"
+                
             />
+            {type === "password" && 
+            <button type="button" className={styles.viewBtn} onClick={togglePasswordVisibility}>
+                {isPasswordVisible ? <IoMdEyeOff/> : <IoMdEye/>}
+            </button>}
             <small className={styles.errorMsg}>{error}</small>
         </div>
     );
